9de2807ec975744e42b1da46523b7d5ec3e4232e
Ricy137 <1932353948@qq.com>
2023-01-05 16:10:15 +0800

    fix: router & ui adjustments

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
 import ClipBoard from "@assets/clipboard.svg";
+import Mark from "@assets/mark.svg";
 
 const Success: React.FC = () => {
   const location = useLocation();
@@ -9,13 +10,14 @@ const Success: React.FC = () => {
     <div className="px-[48px] pt-[42px] flex flex-col justify-start">
       <div className="relative w-[654px] h-[654px]">
         <img alt="cover" src={cover} className="absolute w-[654px] h-[654px] border-[8px] border-[#ffffff] pointer-events-none" draggable={false} />
+        <img alt='mark' src={Mark} className="absolute right-[8px] bottom-[9px] w-[88px] h-[88px]" />
       </div>
       <div className="mt-[42px] flex flex-row w-fit text-[26px] leading-[34px]">
         <div className="py-[3px] px-[12px] rounded-tl-[20px] rounded-bl-[4px] bg-[#6953EF] text-[#ffffff]">通行证ID</div><div className="py-[3px] px-[12px] min-w-[102px] border border-[#6953EF] rounded-tr-[4px] rounded-br-[20px] text-center text-[#6953EF]">{claimed}</div>
       </div>
-      <p className="mt-[24px] font-medium text-[28px] leading-[36px]">哈希</p>
+      <p className="mt-[24px] font-medium text-[28px] leading-[36px] font-semibold">哈希</p>
       <div className="mt-[12px] flex flex-row items-center text-[#696679]">
-        <p>{address}</p>
+        <p className='text-[24px] leading-[32px]'>{address}</p>
         <img src={ClipBoard} alt='clipboard logo' className="ml-[8px] w-[32px] h-[32px] cursor-pointer" onClick={() => navigator.clipboard.writeText(`${address}`)} />
       </div>
       <p className="mt-[40px] text-[40px] leading-[48px] font-semibold">{name}</p>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,11 +9,12 @@ const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RouterWrapper />} />
-        <Route index element={<Home />} />
-        <Route path='home' element={<Home />} />
-        <Route path="success" element={<Success />} />
-        <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/" element={<RouterWrapper />} >
+          <Route index element={<Home />} />
+          <Route path='home' element={<Home />} />
+          <Route path="success" element={<Success />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Route>
       </Routes>
     </Router>
   );
@@ -21,13 +22,15 @@ const AppRouter: React.FC = () => {
 
 const RouterWrapper: React.FC = () => {
   return (
-    <>
+    <div className='flex flex-col min-h-full overflow-hidden'>
       <Navigation />
-      <Outlet />
-      <footer className='mt-[42px] flex flex-row justify-center items-center'>
+      <main className='flex-1 z-10'>
+        <Outlet />
+      </main>
+      <footer className='mt-[42px] flex flex-row justify-center items-center h-[150px]'>
         <img src={Rainbow} alt="Rainbow" className='w-[228px] h-[54px] select-none pointer-events-none' draggable={false} /><span className='ml-[7px]'>提供技术支持</span>
       </footer>
-    </>
+    </div>
   )
 }
 export default AppRouter;
