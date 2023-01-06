3f572b5ca48f07722bd94379fd1c84eb2bceed59
Ricy137 <1932353948@qq.com>
2023-01-06 10:09:16 +0800

    fix: share ui

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -23,7 +23,7 @@ const Success: React.FC = () => {
       <p className="mt-[42px] text-[40px] leading-[48px] font-semibold text-[#05001F]">{name}</p>
       <p className="mt-[24px] text-[28px] text-[#696679] leading-[36px]" dangerouslySetInnerHTML={{ '__html': description }}></p>
       <div className="flex flex-col items-center">
-        <button className="mt-[24px] flex justify-center items-center h-[104px] w-[654px] border border-[#6953EF] rounded-[8px] text-[32px] font-medium leading-[40px] text-[#6953EF]">分享</button>
+        <button className="mt-[24px] flex justify-center items-center h-[104px] w-[654px] rounded-[8px] text-[32px] font-medium leading-[40px] text-[#ffffff] bg-[#6953EF]">分享</button>
       </div>
     </div>
   )
