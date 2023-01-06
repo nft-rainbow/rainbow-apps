fe64994a31a452856d00189506a05d681ce51d03
Ricy137 <1932353948@qq.com>
2023-01-06 14:06:57 +0800

    fix: double font

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -15,7 +15,7 @@ const Success: React.FC = () => {
       <div className="mt-[42px] flex flex-row w-fit h-[40px] text-[26px] leading-[34px]">
         <div className="px-[12px] flex flex-row justify-center items-center h-[40px] rounded-tl-[20px] rounded-bl-[4px] bg-[#6953EF] text-[#ffffff] text-center align-middle">通行证ID</div><div className="px-[12px] flex flex-row justify-center items-center min-w-[102px] h-[40px] border border-[#6953EF] rounded-tr-[4px] rounded-br-[20px] text-center align-middle text-[#6953EF]">{claimed}</div>
       </div>
-      <p className="mt-[24px] font-medium text-[28px] leading-[36px] font-medium text-[#37334C]">哈希</p>
+      <p className="mt-[24px] font-medium text-[28px] leading-[36px] text-[#37334C]">哈希</p>
       <div className="mt-[12px] flex flex-row items-center text-[#696679]">
         <p className='text-[24px] leading-[32px]'>{address}</p>
         <img src={ClipBoard} alt='clipboard logo' className="ml-[8px] w-[32px] h-[32px] cursor-pointer" onClick={() => navigator.clipboard.writeText(`${address}`)} />
