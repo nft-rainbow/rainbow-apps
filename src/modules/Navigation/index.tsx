9946fd2e91b886bb330455186de0ff29daf99dba
Ricy137 <1932353948@qq.com>
2023-01-09 12:40:34 +0800

    fix: navigation ui

--- src/modules/Navigation/index.tsx
+++ src/modules/Navigation/index.tsx
@@ -7,7 +7,7 @@ import { shortenCfxAddress } from "@utils/addressUtils/shortenAddress";
 const Account: React.FC<{ account: string }> = ({ account }) => {
   const shortenA = shortenCfxAddress(account);
   return (
-    <div className="flex flex-row items-center px-[24px] py-[16px] text-[#6953EF] border-[2px] border-[#E6E6E9] rounded-[32px]">
+    <div className="flex flex-row items-center px-[24px] h-[64px] text-[#6953EF] border-[2px] border-[#E6E6E9] rounded-[32px]">
       <img src={Connected} alt="connected logo" className="w-[16px] h-[16px] mr-[16px] select-none" draggable={false} width={16} height={16} />
       <p className="w-[118px] h-[64px] flex justify-center items-center text-[24px] leading-[32px] text-[#05001F] text-ellipsis overflow-hidden">{shortenA}</p>
     </div>
