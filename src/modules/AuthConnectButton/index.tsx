8de4b735d53abd7cf1d861630c100fc9b9d41a5f
Ricy137 <1932353948@qq.com>
2023-01-09 11:12:27 +0800

    feat: shortenCfxAddress

--- src/modules/AuthConnectButton/index.tsx
+++ src/modules/AuthConnectButton/index.tsx
@@ -1,11 +1,7 @@
 import Unconnected from '@assets/unconnected.svg'
 import { useAccount, connect } from "@services/account"
 
-interface ConnectButtonProps {
-  connect: () => void | Promise<void>
-}
-
-const CircleConnectButton: React.FC<ConnectButtonProps> = ({ connect }) => {
+const CircleConnectButton: React.FC = () => {
   return (
     <button onClick={connect} className="flex flex-row items-center px-[24px] py-[16px] text-[#6953EF] border-[2px] border-[#6953EF] rounded-[32px]">
       <img src={Unconnected} alt="unconnected logo" className="w-[16px] h-[16px] mr-[16px] select-none" draggable={false} width={16} height={16} />
@@ -14,7 +10,7 @@ const CircleConnectButton: React.FC<ConnectButtonProps> = ({ connect }) => {
   )
 }
 
-const RectangleConnectButton: React.FC<ConnectButtonProps> = ({ connect }) => {
+const RectangleConnectButton: React.FC = () => {
   return (
     <button onClick={connect} className='mt-[60px] flex justify-center items-center h-[104px] w-[654px] bg-[#6953EF] rounded-[8px] text-[32px] font-medium leading-[40px] text-[#ffffff]'>连接钱包</button>
   )
@@ -24,7 +20,7 @@ const ConnectButton = {
   "circle": CircleConnectButton,
   "rectangle": RectangleConnectButton,
 }
-interface AuthConnectButtonProps  {
+interface AuthConnectButtonProps {
   type: 'circle' | 'rectangle';
   children: JSX.Element | ((account: string) => JSX.Element);
 }
@@ -35,7 +31,7 @@ const AuthConnectButton: React.FC<AuthConnectButtonProps> = ({ type, children })
     if (typeof children === 'function') return children(account);
     return <>{children}</>
   } else {
-    return <>{ConnectButton[type]({ connect })}</>
+    return <>{ConnectButton[type]}</>
   }
 }
 
--- src/modules/Navigation/index.tsx
+++ src/modules/Navigation/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Connected from '@assets/connected.svg'
 import Logo from "@assets/logo.png";
 import AuthConnectButton from "@modules/AuthConnectButton";
+import { shortenCfxAddress } from "@utils/addressUtils/shortenAddress";
 
 const Account: React.FC<{ account: string }> = ({ account }) => {
+  // const shortenA = shortenCfxAddress(account);
+  // console.log('shortenA', shortenA)
   return (
     <div className="flex flex-row items-center px-[24px] py-[16px] text-[#6953EF] border-[2px] border-[#E6E6E9] rounded-[32px]">
       <img src={Connected} alt="connected logo" className="w-[16px] h-[16px] mr-[16px] select-none" draggable={false} width={16} height={16} />
