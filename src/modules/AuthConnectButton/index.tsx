2c3af2f13c358812cbc0f27dca3df134b3915563
Ricy137 <1932353948@qq.com>
2023-01-18 19:30:30 +0800

    fix: AnyWeb

--- src/modules/AuthConnectButton/index.tsx
+++ src/modules/AuthConnectButton/index.tsx
@@ -5,7 +5,7 @@ const CircleConnectButton: React.FC = () => {
   return (
     <button onClick={connect} className="flex flex-row items-center px-[24px] py-[16px] text-[#6953EF] border-[2px] border-[#6953EF] rounded-[32px]">
       <img src={Unconnected} alt="unconnected logo" className="w-[16px] h-[16px] mr-[16px] select-none" draggable={false} width={16} height={16} />
-      连接Anyweb
+      连接AnyWeb
     </button>
   );
 };
