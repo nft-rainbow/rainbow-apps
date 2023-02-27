92257b266952e29e903dfca2738c807e215648a2
Ricy <1932353948@qq.com>
2023-02-27 10:22:16 +0800

    fix: px2vw bug

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,9 @@
-import isMobile from '@utils/isMobie';
-console.log('isMobile', isMobile);
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
     'postcss-px-to-viewport': {
-      viewportWidth: isMobile ? 750 : 1440,
+      viewportWidth: 750,
     },
   },
 };
