fa4683ab4b0a556128339e947d7d13d43e60d484
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-03-20 16:19:15 +0800

    fix: fetchUrl

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -74,7 +74,7 @@ const Home: React.FC = () => {
           </p>
         )}
       </div>
-      {poapConf?.count!== -1 &&
+      {poapConf?.count !== -1 &&
         <p className="md:hidden mt-[32px] text-[28px] leading-[32px] text-[#37334C] align-middle">
           可领取{' '}
           <span className="text-[#6953EF] font-medium mx-[2px]">{loading ? '...' : !!poapConf && poapConf.count && poapConf.count === -1 ? '无限' : poapConf?.count ?? 0}</span> 次
