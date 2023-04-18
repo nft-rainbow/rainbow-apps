9d2881aaeb8abfbf5026b7c6dca1deea842edb67
Like_Frost <1342230493@qq.com>
2023-04-18 15:08:25 +0800

    fix: modal display

--- src/components/showModal/index.tsx
+++ src/components/showModal/index.tsx
@@ -35,7 +35,7 @@ export const ModalRender: React.FC = () => {
   if (!modal) return null;
   
   return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center md:justify-center bg-[rgba(0,0,0,0.6)] z-40">
+    <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center bg-[rgba(0,0,0,0.6)] z-40">
       <div
         className={cx(
           'relative flex flex-col jusity-center w-[90vw] max-w-[560px] p-[28px] md:p-[16px] rounded-[8px] md:rounded-[2px] bg-white overflow-hidden dropdown-shadow z-50',
